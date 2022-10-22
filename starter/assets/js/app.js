c89437568c0a004b8354f81b0e6dbf59064abeee
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2022-10-22 17:04:25 +0100

    ready to serve

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -3,20 +3,14 @@
  * 
  */
 
-// console.log()
-
-document.getElementById('form').reset();
 var todo = document.getElementById("to-do-tasks");
 var inProgress = document.getElementById("in-progress-tasks");
 var done = document.getElementById("done-tasks");
-
-let index;
 
 let NbrTodo = document.getElementById("to-do-tasks-count");
 let NbrIP = document.getElementById("in-progress-tasks-count");
 let NbrDone = document.getElementById("done-tasks-count");
 displayTask();
-saveTask();
 
 function displayTask() {
     let num = 1;
@@ -25,7 +19,7 @@ function displayTask() {
         if (tasks[i].status == "To Do") {
             NbrTodo.innerHTML = ctd;
             todo.innerHTML +=
-                `<button class="task d-flex p-2 border-0 border-top">
+                `<button data-bs-toggle="modal" data-bs-target="#modal" class="task d-flex p-2 border-0 border-top" onclick="editTask(${num - 1})">
                     <div class="col-1">
                         <i class="fa-regular fa-circle-question text-success ms-2 mt-2 fs-4"></i>
                     </div>
@@ -35,9 +29,9 @@ function displayTask() {
                             <div class="text-secondary">#${num} created in ${tasks[i].date}</div>
                             <div title="${tasks[i].description}">${tasks[i].description.slice(0, 50)}...</div>
                         </div>
-                        <div class="d-flex justify-content-between">
-                                <span class="btn btn-primary btn-sm">${tasks[i].priority}</span>
-                                <span class="btn bg-light-600 btn-sm">${tasks[i].type}</span>
+                        <div class="d-flex">
+                            <span class="btn btn-primary btn-sm">${tasks[i].priority}</span>
+                            <span class="btn bg-light-600 btn-sm">${tasks[i].type}</span>
                         </div>
                     </div>
                 </button>`
@@ -47,7 +41,7 @@ function displayTask() {
         else if (tasks[i].status == "In Progress") {
             NbrIP.innerHTML = + cip;
             inProgress.innerHTML +=
-                `<button class="task d-flex p-2 border-0 border-top">
+                `<button data-bs-toggle="modal" data-bs-target="#modal" class="task d-flex p-2 border-0 border-top" onclick="editTask(${num - 1})">
                     <div class="col-1">
                         <i class="fa fa-circle-notch fa-rotate-90 text-success ms-2 mt-2 fs-4"></i>
                     </div>
@@ -57,9 +51,9 @@ function displayTask() {
                             <div class="text-secondary">#${num} created in ${tasks[i].date}</div>
                             <div title="${tasks[i].description}">${tasks[i].description.slice(0, 50)}...</div>
                         </div>
-                        <div class="d-flex justify-content-between">
-                                <span class="btn btn-primary btn-sm">${tasks[i].priority}</span>
-                                <span class="btn bg-light-600 btn-sm">${tasks[i].type}</span>
+                        <div class="d-flex">
+                            <span class="btn btn-primary btn-sm">${tasks[i].priority}</span>
+                            <span class="btn bg-light-600 btn-sm">${tasks[i].type}</span>
                         </div>
                     </div>
                 </button>`
@@ -69,7 +63,7 @@ function displayTask() {
         else if (tasks[i].status == "Done") {
             NbrDone.innerHTML = + cd;
             done.innerHTML +=
-                `<button class="task d-flex p-2 border-0 border-top">
+                `<button data-bs-toggle="modal" data-bs-target="#modal" class="task d-flex p-2 border-0 border-top" onclick="editTask(${num - 1})">
                     <div class="col-1">
                         <i class="fa-regular fa-circle-check text-success ms-2 mt-2 fs-4"></i>
                     </div>
@@ -79,7 +73,7 @@ function displayTask() {
                             <div class="text-secondary">#${num} created in ${tasks[i].date}</div>
                             <div title="${tasks[i].description}">${tasks[i].description.slice(0, 50)}...</div>
                         </div>
-                        <div class="d-flex justify-content-between">
+                        <div class="d-flex">
                             <span class="btn btn-primary btn-sm">${tasks[i].priority}</span>
                             <span class="btn bg-light-600 btn-sm">${tasks[i].type}</span>
                         </div>
@@ -103,51 +97,58 @@ function clearTasks() {
         i.remove();
     }
 }
-num=index;
+let feature = document.getElementById("Feature");
+let bug = document.getElementById("Bug");
+var type;
+let index;
 console.log(index);
 let data = {};
 function createTask() {
-            let title = document.getElementById("title");
-            let priority = document.getElementById("priority");
-            let status = document.getElementById("status");
-            let date = document.getElementById("date");
-            let description = document.getElementById("description");
-            let feature = document.getElementById("Feature");
-            let bug = document.getElementById("Bug");
-            var type;
+    let title = document.getElementById("title");
+    let priority = document.getElementById("priority");
+    let status = document.getElementById("Status");
+    let date = document.getElementById("date");
+    let description = document.getElementById("description");
 
-            if (feature.checked)
-                type = feature.id;
-            else
-                type = bug.id;
+    if (feature.checked)
+        type = feature.id;
+    else
+        type = bug.id;
 
-            let task = {
-                'title': title.value,
-                'type': type,
-                'priority': priority.value,
-                'status': status.value,
-                'date': date.value,
-                'description': description.value,
-            }
-            console.log(task);
-            tasks.push(task);
-        }
+    let task = {
+        'title': title.value,
+        'type': type,
+        'priority': priority.value,
+        'status': status.value,
+        'date': date.value,
+        'description': description.value,
+    }
+    console.log(task);
+    tasks.push(task);
+}
 // Recuperer task attributes a partir les champs input
 
-    // Créez task object   
+// Créez task object   
 
-    // Ajoutez object au Array
+// Ajoutez object au Array
 
-    // refresh tasks
+// refresh tasks
 
+function modalSaveTask() {
+    document.getElementById("save").style.display = 'block';
+    document.getElementById("update").style.display = 'none';
+    document.getElementById("delete").style.display = 'none';
+}
+
+let buttonCancel = document.getElementById("cancel");
 function saveTask() {
-    let form = document.getElementById("BtnSave").addEventListener("click", h);
+    let form = document.getElementById("save").addEventListener("click", h);
+    modalSaveTask();
     function h() {
         console.log("btnclicked");
         formValidation();
     }
     let title = document.getElementById("title");
-    let buttonCancel = document.getElementById("cancel");
 
     let formValidation = () => {
         if (title.value === "") {
@@ -158,63 +159,90 @@ function saveTask() {
         else {
             console.log("success");
             msg.innerHTML = "";
+
             createTask();
             clearTasks();
-            document.getElementById('form').reset();
             buttonCancel.click();
             displayTask();
         }
     }
 }
-    // Initialisez task form
+// Initialisez task form
 
-    // Affichez updates
+// Affichez updates
 
-    // Delete Button
+// Delete Button
 
-    // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
+// Définir l’index en entrée cachée pour l’utiliser en Update et Delete
 
-    // Definir FORM INPUTS
+// Definir FORM INPUTS
 
-    // Ouvrir Modal form
+// Ouvrir Modal form
+
+let x;
 function editTask(index) {
+    x = index;
+    title.value = tasks[index].title;
+    priority.value = tasks[index].priority;
+    description.value = tasks[index].description;
+    date.value = tasks[index].date;
+    Status.value = tasks[index].status;
+
+    if (tasks[index].type == "Bug")
+        bug.checked = true;
+    else
+        feature.checked = true;
+    updateTask();
 }
 
+
+// GET TASK ATTRIBUTES FROM INPUTS
+
+// Créez task object
+
+// Remplacer ancienne task par nouvelle task
+
+// Fermer Modal form
+
+// Refresh tasks
 function updateTask() {
-    // GET TASK ATTRIBUTES FROM INPUTS
+    let newtype;
+    if (feature.checked)
+        newtype = feature.id;
+    else
+        newtype = bug.id;
 
-    // Créez task object
+    let data = {
+        'title': title.value,
+        'type': newtype,
+        'priority': priority.value,
+        'status': Status.value,
+        'date': date.value,
+        'description': description.value,
+    }
 
-    // Remplacer ancienne task par nouvelle task
+    tasks[x] = data;
+    // editTask(x);
+    buttonCancel.click();
+    clearTasks();
 
-    // Fermer Modal form
-
-    // Refresh tasks
+    saveTask();
+    displayTask();
+    document.getElementById("save").style.display = 'none';
+    document.getElementById("update").style.display = 'block';
+    document.getElementById("delete").style.display = 'block';
 
 }
 // Get index of task in the array
-    
-    // Remove task from array by index splice function
 
-    // close modal form
+// Remove task from array by index splice function
 
-    // refresh tasks
-// function deleteTask(index) {
-//     let delete = document.getElementById("delete").addEventListener("click", d);
-//     function d() {
-//         console.log("btnclicked");
-//         index.parentElement.parentElement.remove();
-//     } 
-// }
+// close modal form
 
-function initTaskForm() {
-    // Clear task form from data
-
-    // Hide all action buttons
-}
-
-function reloadTasks() {
-    // Remove tasks elements
-
-    // Set Task count
+// refresh tasks
+function deleteTask() {
+    tasks.splice(x, 1);
+    buttonCancel.click();
+    clearTasks();
+    displayTask();
 }
